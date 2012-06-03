face5cb2f1c080e553853d801cd9b176514f4c60
unknown <remicks@NewGeneration.(none)>
2012-06-03 10:39:58 -0500

    Add new test script

---
+++ build_new.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# $1 should be lunch combo
+# $2 should be device name
+# select device and prepare varibles
+CCACHE=$BUILD_DIR/prebuilt/linux-x86/ccache
+
+export USE_CCACHE=1
+export CCACHE_DIR=/home/$USER/android/.ccache
+cd $CCACHE
+./ccache -M 40G
+BUILD_ROOT=`pwd`
+cd $BUILD_ROOT
+rm -rf out/target/product
+mkdir -p out
+ln -s /tmp/ramdisk/remicks /home/remicks/android/AOKPCB/out/target
+. build/envsetup.sh
+brunch $1 aokpcb_$1_test-$(date +%Y%m%d-%H%M).zip
+
+TARGET_VENDOR=$(echo $TARGET_PRODUCT | cut -f1 -d '_')
+
+# create log dir if not already present
+if test ! -d "$ANDROID_PRODUCT_OUT"
+    echo "$ANDROID_PRODUCT_OUT doesn't exist, creating now"
+    then mkdir -p "$ANDROID_PRODUCT_OUT"
+fi
+
+# build
+if [ "$BACON" = "true" ]; then
+    time make -j6 bacon 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+else
+    time make -j6 otapackage 2>&1 | tee "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log
+fi
+
+# clean out of previous zip
+if [ "$BACON" = "true" ]; then
+    ZIP=$(tail -2 "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f3 -d ' ' | cut -f1 -d ' ' | sed -e '/^$/ d')
+else
+    ZIP=$(grep "Package OTA" "$ANDROID_PRODUCT_OUT"/"$TARGET_PRODUCT"_bot.log | cut -f5 -d '/')
+fi
+
+mkdir -p "$BUILD_ROOT"/upload
+OUTD="$BUILD_ROOT"/upload
+rm $OUTD/$ZIP
+cp "$ANDROID_PRODUCT_OUT"/$ZIP $OUTD/$ZIP
+
+# finish
+echo "$2 build complete"
+
+# md5sum list
+cd $OUTD
+md5sum $ZIP | cat >> "$ZIP".md5sum
+
+cd $BUILD_ROOT
+rm -rf out/target/product
+echo "Complete - Moving to next project"
